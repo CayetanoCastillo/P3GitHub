22cc8e5a01e40c603bc85eae0ad96f9d4e048168
CayetanoCastillo <160121487+CayetanoCastillo@users.noreply.github.com>
2024-03-11 12:13:50 +0100

    multiplicacion y division

--- P3GithuJava/src/main/java/practica3/P3GithuJava.java
+++ P3GithuJava/src/main/java/practica3/P3GithuJava.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.p3githujava;
+package practica3;
 
 import java.util.Scanner;
 
